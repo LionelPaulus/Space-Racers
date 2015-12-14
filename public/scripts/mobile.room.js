5a226004b77ba67b5c2a78394894edb0d164af76
jadefrh <jadefrh@gmail.com>
2015-12-14 22:17:15 +0100

    Add code comments

--- public/scripts/mobile.room.js
+++ public/scripts/mobile.room.js
@@ -1,14 +1,17 @@
+// Quand on a rejoint la room
 socket.on('room:success', function (id) {
 	$('#code-submit').hide();
 	alert('Room joined');
 	$('#end').html('You are in the room '+ id);
 });
 
+// Quand on ne peut pas rejoindre la room
 socket.on('room:error', function (message) {
 	alert(message);
 });
 
 
+// Quand on soumet le code
 $('#code-submit').on('submit', function (e) {
 	e.preventDefault();
 	var code = $('#code').val();
--- src/rooms.js
+++ src/rooms.js
@@ -1,10 +1,17 @@
 var rooms_manager = {
     rooms: {},
 
+    /**
+     * Genere un id de room
+     */
     generateID: function () {
         return Math.round(Math.random() * 10000);
     },
 
+    /**
+     * Cree une room
+     * @param int id
+     */
     create: function (id) {
         this.rooms[id] = {
             players: [],
@@ -12,22 +19,46 @@ var rooms_manager = {
         };
     },
 
+    /**
+     * Verifie si une room existe
+     * @param int id
+     * @return boolean
+     */
     exists: function (id) {
         return (!this.rooms[id]) ? false : true;
     },
 
+    /**
+     * Recupere l'etat d'une room
+     * @param int id
+     * @return boolean
+     */
     getState: function (id) {
         return this.rooms[id].state;
     },
 
+    /**
+     * Ajoute un joueur a une room
+     * @param int id
+     * @param object user
+     */
     addPlayer: function (id, user) {
         this.rooms[id].players[this.countPlayers(id)] = user;
     },
 
+    /**
+     * Compte le nombre de joueur d'une room
+     * @param int id
+     * @return int
+     */
     countPlayers: function (id) {
         return this.rooms[id].players.length;
     },
 
+    /**
+     * Supprime une room
+     * @param int id
+     */
     remove: function (id) {
         delete this.rooms[id];
     }
