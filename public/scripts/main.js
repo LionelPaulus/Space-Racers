3cd0cb2edae70c62f05e15d14fd1e48f0e4aa3b4
Valérian Fauqueur <Valrian Fauqueur>
2015-12-14 17:44:53 +0100

    fix background (lag/size)

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,8 +1,11 @@
 var canvas = document.getElementById("game");
 var ctx = canvas.getContext("2d");
 
-var canvas_height = canvas.height;
-var canvas_width  = canvas.width;
+var canvas_size = getViewport();
+var canvas_width  = canvas_size[0];
+var canvas_height = canvas_size[1];
+canvas.setAttribute("width", canvas_width);
+canvas.setAttribute("height", canvas_height);
 
 var stars = [];
 
@@ -10,9 +13,9 @@ function createStars()
 {
     var star = {};
     star.x = rNumber(0,canvas_width);
-    star.y = -5;
-    star.size = rNumber(1,3);
-    star.speed = star.size*0.25;
+    star.y = 0;
+    star.size = rNumber(1,8);
+    star.speed = star.size*0.5;
     star.style = "white";
     stars.push(star);
 };
@@ -22,8 +25,7 @@ function updateStars()
     for(var i = 0; i < stars.length; i++)
     {
         var star = stars[i];
-        star.y += star.speed;
-
+        star.y = Math.round(star.y + star.speed);
         if (star.y > canvas_height)
         {
             stars.splice(i,1);
@@ -48,6 +50,7 @@ function draw()
     updateStars();
 }
 
+draw();
 
 
 
@@ -66,4 +69,31 @@ function clear()
     ctx.clearRect(0,0,canvas.width,canvas.height);
 }
 
-draw();
+
+function getViewport() {
+
+ var viewPortWidth;
+ var viewPortHeight;
+
+ // the more standards compliant browsers (mozilla/netscape/opera/IE7) use window.innerWidth and window.innerHeight
+ if (typeof window.innerWidth != 'undefined') {
+   viewPortWidth = window.innerWidth,
+   viewPortHeight = window.innerHeight
+ }
+
+// IE6 in standards compliant mode (i.e. with a valid doctype as the first line in the document)
+ else if (typeof document.documentElement != 'undefined'
+ && typeof document.documentElement.clientWidth !=
+ 'undefined' && document.documentElement.clientWidth != 0) {
+    viewPortWidth = document.documentElement.clientWidth,
+    viewPortHeight = document.documentElement.clientHeight
+ }
+
+ // older versions of IE
+ else {
+   viewPortWidth = document.getElementsByTagName('body')[0].clientWidth,
+   viewPortHeight = document.getElementsByTagName('body')[0].clientHeight
+ }
+ return [viewPortWidth, viewPortHeight];
+}
+
