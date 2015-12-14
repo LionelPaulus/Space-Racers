a73c8ef6bff31d9c618e930efb3d43c72ab2ce0b
jadefrh <jadefrh@gmail.com>
2015-12-14 22:18:07 +0100

    Add code comments

--- public/scripts/mobile.room.js
+++ public/scripts/mobile.room.js
@@ -1,20 +1,20 @@
 // Quand on a rejoint la room
 socket.on('room:success', function (id) {
-	$('#code-submit').hide();
-	alert('Room joined');
-	$('#end').html('You are in the room '+ id);
+    $('#code-submit').hide();
+    alert('Room joined');
+    $('#end').html('You are in the room '+ id);
 });
 
 // Quand on ne peut pas rejoindre la room
 socket.on('room:error', function (message) {
-	alert(message);
+    alert(message);
 });
 
 
 // Quand on soumet le code
 $('#code-submit').on('submit', function (e) {
-	e.preventDefault();
-	var code = $('#code').val();
+    e.preventDefault();
+    var code = $('#code').val();
 
-	socket.emit('room:join', code);
+    socket.emit('room:join', code);
 });
