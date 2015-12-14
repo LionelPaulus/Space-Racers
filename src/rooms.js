03023ed606d4b49d0d17c8598d14e62e4c3eab28
jadefrh <jadefrh@gmail.com>
2015-12-14 22:14:14 +0100

    Convert indentation to space

--- src/rooms.js
+++ src/rooms.js
@@ -1,36 +1,36 @@
 var rooms_manager = {
-	rooms: {},
+    rooms: {},
 
-	generateID: function () {
-		return Math.round(Math.random() * 10000);
-	},
+    generateID: function () {
+        return Math.round(Math.random() * 10000);
+    },
 
-	create: function (id) {
-		this.rooms[id] = {
-			players: [],
-			state: false
-		};
-	},
+    create: function (id) {
+        this.rooms[id] = {
+            players: [],
+            state: false
+        };
+    },
 
-	exists: function (id) {
-		return (!this.rooms[id]) ? false : true;
-	},
+    exists: function (id) {
+        return (!this.rooms[id]) ? false : true;
+    },
 
-	getState: function (id) {
-		return this.rooms[id].state;
-	},
+    getState: function (id) {
+        return this.rooms[id].state;
+    },
 
-	addPlayer: function (id, user) {
-		this.rooms[id].players[this.countPlayers(id)] = user;
-	},
+    addPlayer: function (id, user) {
+        this.rooms[id].players[this.countPlayers(id)] = user;
+    },
 
-	countPlayers: function (id) {
-		return this.rooms[id].players.length;
-	},
+    countPlayers: function (id) {
+        return this.rooms[id].players.length;
+    },
 
-	remove: function (id) {
-		delete this.rooms[id];
-	}
+    remove: function (id) {
+        delete this.rooms[id];
+    }
 };
 
 module.exports = rooms_manager;
