667fada04abf43741a6e0ee902692cb3eb4eb582
jadefrh <jadefrh@gmail.com>
2015-12-15 16:29:04 +0100

    Add gyroscrope test server

---
+++ gyroserv.js
@@ -0,0 +1,40 @@
+var express = require('express'); //EXPRESS c'est un framework qui va juste servir a servir le site statique aux utilisateur
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+var port = process.env.PORT || 3000; // Process c'est pour heroku car le port peut changer
+
+var gyroscope = null;
+var game = null;
+var startInfos = {};
+
+// Lorsqu'un utilisateur se connecte au serv
+io.on('connection', function(socket) {
+    
+    socket.on('gyroscope', function () {
+        gyroscope = game;
+
+        if (game != null) socket.emit('init', startInfos);
+    });
+
+    // client width
+    // client height
+    // position start (x, y)
+    socket.on('game', function (infos) {
+        game = socket;
+        startInfos = JSON.parse(infos);
+
+        if (gyroscope != null) socket.emit('init', startInfos);
+    });
+
+    socket.on('position', function (pos) {
+        game.emit('position', pos);
+    });
+
+});
+
+
+// On lance le serveur sur le port 3000
+server.listen(port, function () {
+    console.log('Server listening at port %d', port);
+});
